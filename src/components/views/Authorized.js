b80146356b0d092686f06cb1a72e166944495461
James Scott <jsa.codes@gmail.com>
2022-12-13 22:58:11 -0600

    Fixed issue in Authorized.js

--- src/components/views/Authorized.js
+++ src/components/views/Authorized.js
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom"
 
-export const Authorized = ({ token }) => {
-  if (localStorage.getItem("pj_token")) {
+export const Authorized = () => {
+  if (localStorage.getItem("auth_token")) {
     return <Outlet />
   }
   return <Navigate to='/login' replace />
