a1d196603f6bd9024b341615f59885b60aa729d2
James Scott <jsa.codes@gmail.com>
2022-12-14 15:11:14 -0600

    Created home page and landing page. Considering some different ideas for how users first interact with the site

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { Login } from "../auth/Login"
 import { RegisterInstructor } from '../auth/RegisterInstructor'
 import { RegisterStudent } from '../auth/RegisterStudent'
+import { HomePage } from '../home/HomePage'
 import { InstructorList } from '../instructors/InstructorList'
 import { InstructorProfile } from '../instructors/InstructorProfile'
 import { StudentList } from '../students/StudentList'
@@ -14,6 +15,7 @@ export const ApplicationViews = () => {
   return <>
     <Routes>
 
+      <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register-instructor" element={<RegisterInstructor />} />
       <Route path="/register-student" element={<RegisterStudent />} />
