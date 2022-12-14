71e8191e59e1428b67762439149cbe21b5d8f253
James Scott <jsa.codes@gmail.com>
2022-12-14 15:05:35 -0600

    Renamed files for clarity

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -3,7 +3,9 @@ import { Login } from "../auth/Login"
 import { RegisterInstructor } from '../auth/RegisterInstructor'
 import { RegisterStudent } from '../auth/RegisterStudent'
 import { InstructorList } from '../instructors/InstructorList'
+import { InstructorProfile } from '../instructors/InstructorProfile'
 import { StudentList } from '../students/StudentList'
+import { StudentProfile } from '../students/StudentProfile'
 import { Authorized } from "./Authorized"
 
 
@@ -17,7 +19,9 @@ export const ApplicationViews = () => {
       <Route path="/register-student" element={<RegisterStudent />} />
       <Route element={<Authorized />} />
       <Route path="/students" element={<StudentList />} />
+      <Route path="/students/:studentId" element={<StudentProfile />} />
       <Route path="/instructors" element={<InstructorList />} />
+      <Route path="/instructors/:instructorId" element={<InstructorProfile />} />
     </Routes>
   </>
 }
