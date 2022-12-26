2a337abeebf2cf64e29d3ab8cb27799e529b792c
James Scott <jsa.codes@gmail.com>
2022-12-26 11:55:07 -0600

    Created Student Manager. Completed functions for GET ALL GET single UPDATE and DELETE student

--- src/components/managers/StudentManager.js
+++ src/components/managers/StudentManager.js
@@ -1,20 +1,45 @@
 export const getStudents = () => {
     return fetch("http://localhost:8000/students", {
         headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
             "Authorization": `Token ${localStorage.getItem("pj_token")}`
         }
     })
         .then(res => res.json())
 }
 
-export const getStudentById = (id) => {
-
+export const getSingleStudent = (id) => {
+    return fetch(`http://localhost:8000/students/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("pj_token")}`
+        }
+    })
+        .then(res => res.json())
 }
 
-export const updateStudent = () => {
-
+export const updateStudent = (student) => {
+    return fetch(`http://localhost:8000/students/${student}`, {
+        method: "PUT",
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json",
+            "Authorization": `Token ${'pj_token'}`
+        },
+        body: JSON.stringify(student)
+    })
 }
 
-export const deleteStudent = () => {
-
+export const deleteStudent = (id) => {
+    return fetch(`http://localhost:8000/students/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Token ${localStorage.getItem("pj_token")}`
+        }
+    })
 }
